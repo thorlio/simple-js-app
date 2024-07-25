1cee1c8bbfe057fb8f8121a4d86d0a9e9d81bc1d
thorlio <thorlio3@gmail.com>
2024-07-24 23:28:33 -0700

    practice
    exercise 1.1 practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
+// this is an alert pop up once the page loads
 alert('Hello world');
+
+// let myName = 'Bob';
+// document.write(myName);
+
+// let myName = 'Sandy';
+// document.write(myName);
+
+// myName = "Maiya";
+// document.write(myName);
+
+// let simpleAddition = 2 + 2;
+// document.write(simpleAddition);
+
+// let size = 100;
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+let tired = 'working hard';
+document.write(tired);
