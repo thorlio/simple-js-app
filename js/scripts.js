83bb474abcd940906b9b9bd20bfeabe5b34254e0
thorlio <thorlio3@gmail.com>
2024-07-28 22:32:43 -0700

    Update scripts.js
    added pokemons to pokedex project

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [{name: "Bulbasaur"}, {height: 0.7}, {type: ["grass", "poison"]},
+{name: "Squirtle"}, {height: 0.5}, {type: "water"},
+{name: "Snorlax"}, {height: 2.1}, {type: "normal"}];
+
+console.log(pokemonList);
