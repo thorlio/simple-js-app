56480a16f06660f8bfd445f37d23daf22c5c4c73
thorlio <thorlio3@gmail.com>
2024-07-31 22:38:18 -0700

    Updated bg color and js
    Added pokemon info into scrips.js file
    added bg color and padding on css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-let pokemonList = [{name: "Bulbasaur"}, {height: 0.7}, {type: ["grass", "poison"]},
-{name: "Squirtle"}, {height: 0.5}, {type: "water"},
-{name: "Snorlax"}, {height: 2.1}, {type: "normal"}];
-
-console.log(pokemonList);
+let pokemonList = [
+  {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
+  {name: "Squirtle", height: 0.5, type: "water"},
+  {name: "Snorlax", height: 2.1, type: "normal"}
+  ];
+  
+  for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 0.6) {
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - It's tiny.<br>");
+    } else if (pokemonList[i].height >= 0.7 && pokemonList[i].height < 0.8) {
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+    } else {
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!")
+    }
+  }
+  
