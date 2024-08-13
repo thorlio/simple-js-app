5c883bcac2b4f50c8ea77387bd67ab57cc5d31a8
thorlio <thorlio3@gmail.com>
2024-08-12 21:35:43 -0700

    Updated Pokedex
    Added styling and a list for the pokedex entries.
    Used querySelector, append, create, and eventlistener functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,63 @@
-let pokemonRepository = (function() {
-  let pokemonList = [
-    {name: "Bulbasaur", height: 0.7, type: ["grass", "poison"]},
-    {name: "Squirtle", height: 0.5, type: "water"},
-    {name: "Snorlax", height: 2.1, type: "normal"},
-    {name: "Psyduck", height: 0.8, type: "water"},
-    {name: "Onix", height: 8.8, type: ["rock", "ground"]},
-    {name: "Charmeleon", height: 1.1, type: "fire"}
-  ];
 
-  // for (let i = 0; i < pokemonList.length; i++) {
-  //   if (pokemonList[i].height < 0.6) {
-  //     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - It's tiny.<br>");
-  //   } else if (pokemonList[i].height >= 0.7 && pokemonList[i].height < 0.8) {
-  //     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
-  //   } else {
-  //     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!")
-  //   }
-  // }
-  
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height < 0.6) {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - It's tiny.<br>");
+//   } else if (pokemonList[i].height >= 0.7 && pokemonList[i].height < 0.8) {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+//   } else {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!")
+//   }
+// }
+
+
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+    { name: "Squirtle", height: 0.5, type: "water" },
+    { name: "Snorlax", height: 2.1, type: "normal" },
+    { name: "Psyduck", height: 0.8, type: "water" },
+    { name: "Onix", height: 8.8, type: ["rock", "ground"] },
+    { name: "Charmeleon", height: 1.1, type: "fire" }
+  ];
 
   function getAll() {
     return pokemonList;
   }
 
-  return {
-    add: add,
-    getAll: getAll
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-})();
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  };
+
+  function addListItem(pokemon) {
+    let pokemonListOf = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('poke-button');
+    listItem.appendChild(button);
+    pokemonListOf.appendChild(listItem);
+    listItem.classList.add('pokemonButton');
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+
+  };
+
+  return {
+    getAll,
+    add,
+    addListItem,
+  };
+
+}) ();
 
 
-pokemonRepository.add({name: 'Pikachu', height: 0.4, type: 'electric'});
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, type: 'electric' });
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write('<p>' + pokemon.name + ' is ' + pokemon.height + ' m tall');
-  if (pokemon.height > 2) {
-    document.write(" - Wow, that's big!");
-  }
+  pokemonRepository.addListItem(pokemon);
 });
